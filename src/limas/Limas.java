3bd8e112a80b73781993fa532ec5c9002e735fb0
Winder <117703396+W1nd3rr@users.noreply.github.com>
2023-04-20 16:44:19 +1100

    Add files via upload
    Program Untuk mencari luas permukaan dan volume suatu kubus dari inputan user

---
+++ src/limas/Limas.java
@@ -0,0 +1,17 @@
+
+package limas;
+
+/**
+ *
+ * @author Pavilion
+ */
+public class Limas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Program untuk menghitung luas permukaan dan volume limas jika nilai tinggi,luas alas dan sisi tegak diinput oleh User
+    }
+    
+}
